bd7a3b5ca0bb582cb6dade8ad29493a946fc69ad
liuruijie <liuruijie@doumi.com>
2018-04-04 17:39:12 +0800

    路由测试

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,6 +39,8 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
+        $this->mapTestRoutes();
+
         //
     }
 
@@ -69,5 +71,19 @@ class RouteServiceProvider extends ServiceProvider
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
+    }
+
+    /**
+     * Define the "api" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapTestRoutes()
+    {
+        Route::prefix('test')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/test.php'));
     }
 }
